563285582544383ba82876f563d6feed3ac7e602
Thomas Gravgaard <fehaar@gosuman.dk>
2010-11-01 13:33:20 +0100

    We can now display a model

---
+++ PSDImporter/PSDImporter.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.ComponentModel;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Graphics.PackedVector;
+
+using PhotoshopFile;
+
+namespace PSDImporter
+{
+	/// <summary>
+	/// This class will be instantiated by the XNA Framework Content Pipeline
+	/// to import a file from disk into the specified type, TImport.
+	/// 
+	/// This should be part of a Content Pipeline Extension Library project.
+	/// 
+	/// TODO: change the ContentImporter attribute to specify the correct file
+	/// extension, display name, and default processor for this importer.
+	/// </summary>
+	[ContentImporter(".psd", DisplayName = "Photoshop .PSD Importer", DefaultProcessor = "TextureProcessor")]
+	public class PSDImporter : ContentImporter<Texture2DContent>
+	{
+
+		public override Texture2DContent Import(string filename, ContentImporterContext context)
+		{
+			PsdFile psdFile = new PsdFile();
+			psdFile.Load(filename);
+
+			Texture2DContent content = new Texture2DContent();
+            PixelBitmapContent<Byte4> bitmap = new PixelBitmapContent<Byte4>(psdFile.Columns, psdFile.Rows);
+
+			byte[] pixelData = new byte[bitmap.Width * bitmap.Height * 4];
+			List<Color[]> layers = new List<Color[]>();
+
+			//Debugger.Launch();
+
+			// Extract all the layers
+			foreach (Layer layer in psdFile.Layers)
+			{
+				Color[] layerData = new Color[psdFile.Columns * psdFile.Rows];
+				ExtractLayer(layer, layerData);
+				layers.Add(layerData);
+			}
+
+			// Flatten the layers
+			for (int i = layers.Count - 1; i > 0; i--)
+				FlattenLayers(psdFile.Layers[i], psdFile.Layers[i - 1], layers[i], layers[i - 1]);
+
+			ColorToByte(layers[0], pixelData);
+
+			bitmap.SetPixelData(pixelData);
+
+			content.Mipmaps.Add(bitmap);
+
+			return content;
+		}
+
+		private void ColorToByte(Color[] colorData, byte[] byteData)
+		{
+			int byteIndex = 0;
+			for (int i = 0; i < colorData.Length; i++)
+			{
+				byteData[byteIndex] = colorData[i].R;
+				byteData[byteIndex + 1] = colorData[i].G;
+				byteData[byteIndex + 2] = colorData[i].B;
+				byteData[byteIndex + 3] = colorData[i].A;
+
+				byteIndex += 4;
+			}
+		}
+
+		private void FlattenLayers(Layer layer0, Layer layer1, Color[] layerData0, Color[] layerData1)
+		{
+			for (int i = 0; i < layerData1.Length; i++)
+				layerData1[i] = Blend(layerData0[i], layerData1[i]);
+		}
+
+		private void ExtractLayer(Layer layer, Color[] layerData)
+		{			
+			int pixelIndex = 0;
+			for (int y = layer.Rect.Top; y < layer.Rect.Bottom; y++)
+			{
+				for (int x = layer.Rect.Left; x < layer.Rect.Right; x++)
+				{
+					Color color = Color.Black;
+
+					foreach (Layer.Channel channel in layer.Channels)
+					{
+						// For now only take RGBA channels
+						if (channel.ID == 0)
+							color.R = channel.ImageData[pixelIndex];
+						else if (channel.ID == 1)
+							color.G = channel.ImageData[pixelIndex];
+						else if (channel.ID == 2)
+							color.B = channel.ImageData[pixelIndex];
+						else if (channel.ID == -1)
+							color.A = channel.ImageData[pixelIndex];
+					}
+
+					layerData[y * layer.PsdFile.Columns + x] = color;
+
+					pixelIndex++;
+				}
+			}
+		}
+
+		private Color Blend(Color source, Color destination)
+		{
+			float alpha = source.A / 255.0f;
+			float invAlpha = 1.0f - alpha;
+			Color result = Color.Transparent;
+			result.R = (byte)(source.R * alpha + destination.R * invAlpha);
+			result.G = (byte)(source.G * alpha + destination.G * invAlpha);
+			result.B = (byte)(source.B * alpha + destination.B * invAlpha);
+			result.A = ClampByte((int)source.A + (int)destination.A);
+
+			return result;
+		}
+
+		private byte ClampByte(int value)
+		{
+			if (value > 255)
+				return 255;
+			else if (value < 0)
+				return 0;
+			else
+				return (byte)value;
+		}
+	}
+}
--- PressPlay.Tentacles.Win/PressPlay.Tentacles.Win/Game1.cs
+++ PressPlay.Tentacles.Win/PressPlay.Tentacles.Win/Game1.cs
@@ -19,6 +19,11 @@ namespace PressPlay.Tentacles.Win
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        Model model;
+        Matrix[] boneTransforms;
+        Texture2D tex;
+        float rotationXAmount = 0;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -48,6 +53,10 @@ namespace PressPlay.Tentacles.Win
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            model = Content.Load<Model>("Levels/Models/block_generic_filler01");
+            boneTransforms = new Matrix[model.Bones.Count];
+
+            tex = Content.Load<Texture2D>("Levels/Maps/block_brown_desat");
         }
 
         /// <summary>
@@ -71,6 +80,7 @@ namespace PressPlay.Tentacles.Win
                 this.Exit();
 
             // TODO: Add your update logic here
+            rotationXAmount += 10;
 
             base.Update(gameTime);
         }
@@ -81,11 +91,34 @@ namespace PressPlay.Tentacles.Win
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            //// TODO: Add your drawing code here
+            // Set the world matrix as the root transform of the model.
+            model.Root.Transform = Matrix.CreateWorld(new Vector3(0, 0, 0), Vector3.Forward, Vector3.Up);
+
+            // Look up combined bone matrices for the entire model.
+            model.CopyAbsoluteBoneTransformsTo(boneTransforms);
+
+            // Draw the model.
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = boneTransforms[mesh.ParentBone.Index];
+                    effect.View = Matrix.CreateLookAt(new Vector3(1, 5000, 1), Vector3.Zero, Vector3.Up);
+                    effect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), GraphicsDevice.Viewport.AspectRatio, 10, 20000);
+                    effect.EnableDefaultLighting();
+                    effect.Texture = tex;
+                    //SetEffectLights(effect, Lights);
+                    //SetEffectPerPixelLightingEnabled(effect);
+                    effect.TextureEnabled = true;
+                }
+                mesh.Draw();
+            }
 
             base.Draw(gameTime);
         }
+
     }
 }
