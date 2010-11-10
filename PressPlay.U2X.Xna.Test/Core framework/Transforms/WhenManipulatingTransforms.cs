dcdb6b41aa0603cfcc6c54f078a247c2a2e083b6
Thomas Gravgaard <fehaar@gosuman.dk>
2010-11-10 12:32:03 +0100

    We can now move a game object relative to its parent.

---
+++ PressPlay.U2X.Xna.Test/Core framework/Transforms/WhenManipulatingTransforms.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Microsoft.Xna.Framework;
+
+namespace PressPlay.U2X.Xna.Test.Core_framework.Transforms
+{
+    [TestFixture]
+    public class WhenManipulatingTransforms
+    {
+        [Test]
+        public void AChildWillBePositionedRelativeToItsParent()
+        {
+            Transform trans = new Transform() { localPosition = Vector3.One };
+            Transform child = new Transform() { localPosition = Vector3.Up };
+
+            Assert.That(child.position, Is.EqualTo(Vector3.Up));
+            child.parent = trans;
+            Assert.That(child.position, Is.EqualTo(Vector3.One + Vector3.Up));
+        }
+
+        [Test]
+        public void MovingAParentWillMoveItsChild()
+        {
+            Transform trans = new Transform() { localPosition = Vector3.One };
+            Transform child = new Transform() { localPosition = Vector3.Up };
+            child.parent = trans;
+            Assert.That(child.position, Is.EqualTo(Vector3.One + Vector3.Up));
+            trans.localPosition = new Vector3(2, 2, 2);
+            Assert.That(child.position, Is.EqualTo(new Vector3(2, 2, 2) + Vector3.Up));
+        }
+	
+	
+    }
+}
--- PressPlay.U2X.Xna.Test/Core framework/WhenLoadingAScene.cs
+++ PressPlay.U2X.Xna.Test/Core framework/WhenLoadingAScene.cs
@@ -11,17 +11,33 @@ namespace PressPlay.U2X.Xna.Test.Core_framework
     {
         Scene scene;
         GameObject go;
+        GameObject child;
+        GameObject childOfChild;
         TestComponent component;
+        TestComponent childComponent;
+        TestComponent childOfChildComponent;
 
         [SetUp]
         public void Setup()
         {
             scene = new Scene();
-            go = new GameObject();
+            go = new GameObject() { transform = new Transform() };
             component = new TestComponent();
 
             go.components.Add(component);
             scene.gameObjects.Add(go);
+
+            child = new GameObject() { transform = new Transform() };
+            childComponent = new TestComponent();
+            child.components.Add(childComponent);
+            go.transform.children = new List<GameObject>();
+            go.transform.children.Add(child);
+
+            childOfChild = new GameObject() { transform = new Transform() };
+            childOfChildComponent = new TestComponent();
+            childOfChild.components.Add(childOfChildComponent);
+            child.transform.children = new List<GameObject>();
+            child.transform.children.Add(childOfChild);
         }
 
         [Test]
@@ -32,6 +48,37 @@ namespace PressPlay.U2X.Xna.Test.Core_framework
             Assert.That(component.gameObject, Is.Not.Null);
             Assert.That(component.gameObject, Is.EqualTo(go));
         }
-	
+
+        [Test]
+        public void AfterLoadWillEnsureThatAllReferencesAreSetOnComponentsAllTheWayThroughTheHierachy()
+        {
+            Assert.That(childComponent.gameObject, Is.Null);
+            Assert.That(childOfChildComponent.gameObject, Is.Null);
+            scene.AfterLoad();
+            Assert.That(childComponent.gameObject, Is.Not.Null);
+            Assert.That(childComponent.gameObject, Is.EqualTo(child));
+            Assert.That(childOfChildComponent.gameObject, Is.Not.Null);
+            Assert.That(childOfChildComponent.gameObject, Is.EqualTo(childOfChild));
+        }
+
+        [Test]
+        public void AfterLoadWillEnsureThatAllTransformParentReferencesAreSet()
+        {
+            Assert.That(child.transform.parent, Is.Null);
+            scene.AfterLoad();
+            Assert.That(child.transform.parent, Is.Not.Null);
+            Assert.That(child.transform.parent, Is.SameAs(go.transform));
+        }
+
+
+        [Test]
+        public void AfterLoadWillEnsureThatAllTransformParentReferencesAreSetAllTheWayThroughTheHierachy()
+        {
+            Assert.That(childOfChild.transform.parent, Is.Null);
+            scene.AfterLoad();
+            Assert.That(childOfChild.transform.parent, Is.Not.Null);
+            Assert.That(childOfChild.transform.parent, Is.SameAs(child.transform));
+        }
+
     }
 }
