d82aca42307f596d1436a2f14635e95a9c1508b1
Klaus Gregersen <klaus@pressplay.dk>
2011-01-19 13:09:28 +0100

    hacked resources to cache loaded objects. This is ONLY A TEMPORARY SOLUTION

--- PressPlay.FFWD/Resources.cs
+++ PressPlay.FFWD/Resources.cs
@@ -8,13 +8,27 @@ namespace PressPlay.FFWD
 {
     public static class Resources
     {
+        private static Dictionary<string, GameObject> cachedResources = new Dictionary<string, GameObject>();
+
+
         public static UnityObject Load(string name)
         {
+            //TODO: remove this when loaded resources are handled correctly
+            if (cachedResources.ContainsKey(name)) 
+            {
+                return cachedResources[name];
+            }
+
             Application.loadingScene = true;
             GameObject go = ContentHelper.Content.Load<GameObject>(Path.Combine("Resources", name));
             go.AfterLoad(null);
             go.SetNewId(null);
             Application.loadingScene = false;
+
+            //TODO: remove this when loaded resources are handled correctly
+            GameObject.DontDestroyOnLoad(go);
+            cachedResources.Add(name,go);
+
             return go;
         }
     }
