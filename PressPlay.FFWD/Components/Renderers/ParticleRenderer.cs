5413a9dd04d310e3789985e8902fb96e643e9f17
Thomas Gravgaard <thomas@pressplay.dk>
2011-02-09 13:12:06 +0100

    Now pregenerating the indexbuffer and reusing it. It will never change anyway.

--- PressPlay.FFWD/Components/Renderers/ParticleRenderer.cs
+++ PressPlay.FFWD/Components/Renderers/ParticleRenderer.cs
@@ -42,7 +42,20 @@ namespace PressPlay.FFWD.Components
             if (vertexBuffer == null && (emitter.particles != null))
             {
                 vertexBuffer = new DynamicVertexBuffer(Application.screenManager.GraphicsDevice, typeof(VertexPositionColorTexture), emitter.particles.Length * 4, BufferUsage.WriteOnly);
-                indexBuffer = new DynamicIndexBuffer(Application.screenManager.GraphicsDevice, IndexElementSize.SixteenBits, emitter.particles.Length * 6, BufferUsage.WriteOnly);
+                indexBuffer = new IndexBuffer(Application.screenManager.GraphicsDevice, IndexElementSize.SixteenBits, emitter.particles.Length * 6, BufferUsage.WriteOnly);
+
+                triangles = new short[emitter.particles.Length * 6];
+                int j = 0;
+                for (int i = 0; i < emitter.particles.Length * 6; i += 6, j += 4)
+                {
+                    triangles[i] = (short)j;
+                    triangles[i + 1] = (short)(j + 2);
+                    triangles[i + 2] = (short)(j + 1);
+                    triangles[i + 3] = (short)(j + 1);
+                    triangles[i + 4] = (short)(j + 2);
+                    triangles[i + 5] = (short)(j + 3);
+                }
+                indexBuffer.SetData(triangles, 0, triangles.Length);
             }
         }
 
@@ -86,7 +99,6 @@ namespace PressPlay.FFWD.Components
             if (vertices == null)
 	        {
                 vertices = new VertexPositionColorTexture[emitter.particles.Length * 4];
-                triangles = new short[emitter.particles.Length * 6];
 	        }
 
             int particlesRendered = 0;
@@ -99,7 +111,6 @@ namespace PressPlay.FFWD.Components
                 }
             }
             vertexBuffer.SetData(vertices, 0, particlesRendered * 4);
-            indexBuffer.SetData(triangles, 0, particlesRendered * 6);
 
             device.Indices = indexBuffer;
             device.SetVertexBuffer(vertexBuffer);
@@ -144,13 +155,6 @@ namespace PressPlay.FFWD.Components
             vertices[vertexIndex + 3].TextureCoordinate = new Microsoft.Xna.Framework.Vector2(1, 0);
             vertices[vertexIndex + 3].Position = pos + (Vector3)new Vector2(size, -size);
             vertices[vertexIndex + 3].Color = particle.Color;
-
-            triangles[triangleIndex] = (short)vertexIndex;
-            triangles[triangleIndex + 1] = (short)(vertexIndex + 2);
-            triangles[triangleIndex + 2] = (short)(vertexIndex + 1);
-            triangles[triangleIndex + 3] = (short)(vertexIndex + 1);
-            triangles[triangleIndex + 4] = (short)(vertexIndex + 2);
-            triangles[triangleIndex + 5] = (short)(vertexIndex + 3);
         }
 
     }
