1d0e1afbaf14e36a4fabab378feb67d3bee9fcc2
Thomas Gravgaard <thomas@pressplay.dk>
2011-03-15 15:07:37 +0100

    Fix errors in test project. Finish simplifying meshcolldiers

--- PressPlay.FFWD.Test/Physics facts/WhenPerformingARaycast.cs
+++ PressPlay.FFWD.Test/Physics facts/WhenPerformingARaycast.cs
@@ -21,7 +21,7 @@ namespace PressPlay.FFWD.Test.Physics_facts
         public void WeCanDetermineAColliderHit()
         {
             Body body = Physics.AddBody();
-            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 0, 1);
+            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 1);
             bool hit = Physics.Raycast(Vector2.zero, Vector2.up, 100, 0);
             Assert.That(hit, Is.True);
         }
@@ -30,7 +30,7 @@ namespace PressPlay.FFWD.Test.Physics_facts
         public void WeCanDetermineAColliderHitUsing3d()
         {
             Body body = Physics.AddBody();
-            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 0, 1);
+            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 1);
             bool hit = Physics.Raycast(Vector3.zero, -Vector3.forward, 100, 0);
             Assert.That(hit, Is.True);
         }
@@ -39,7 +39,7 @@ namespace PressPlay.FFWD.Test.Physics_facts
         public void WeCanDetermineAColliderHitUsingRay()
         {
             Body body = Physics.AddBody();
-            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 0, 1);
+            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 1);
             Ray ray = new Ray(Vector3.zero, -Vector3.forward);
             bool hit = Physics.Raycast(ray, 100, 0);
             Assert.That(hit, Is.True);
@@ -49,7 +49,7 @@ namespace PressPlay.FFWD.Test.Physics_facts
         public void WeCanMissAllColliders()
         {
             Body body = Physics.AddBody();
-            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 0, 1);
+            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 1);
             bool hit = Physics.Raycast(Vector2.zero, Vector2.right, 100, 0);
             Assert.That(hit, Is.False);
         }
@@ -58,7 +58,7 @@ namespace PressPlay.FFWD.Test.Physics_facts
         public void WeCanMissAllCollidersUsing3d()
         {
             Body body = Physics.AddBody();
-            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 0, 1);
+            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 1);
             bool hit = Physics.Raycast(Vector2.zero, Vector2.right, 100, 0);
             Assert.That(hit, Is.False);
         }
@@ -67,7 +67,7 @@ namespace PressPlay.FFWD.Test.Physics_facts
         public void WeCanMissAllCollidersUsingRay()
         {
             Body body = Physics.AddBody();
-            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 0, 1);
+            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 1);
             Ray ray = new Ray(Vector3.zero, Vector3.right);
             bool hit = Physics.Raycast(ray, 100, 0);
             Assert.That(hit, Is.False);
@@ -77,7 +77,7 @@ namespace PressPlay.FFWD.Test.Physics_facts
         public void WeWillNotHitAColliderIfTheRayStartsInsideIt()
         {
             Body body = Physics.AddBody();
-            Physics.AddBox(body, false, 10, 10, new Vector2(0, 0), 0, 1);
+            Physics.AddBox(body, false, 10, 10, new Vector2(0, 0), 1);
             bool hit = Physics.Raycast(Vector2.zero, Vector2.up, 100, 0);
             Assert.That(hit, Is.False);
         }
@@ -86,7 +86,7 @@ namespace PressPlay.FFWD.Test.Physics_facts
         public void WeCanGetHitInfoOnTheObjectThatWasHit()
         {
             Body body = Physics.AddBody();
-            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 0, 1);
+            Physics.AddBox(body, false, 10, 10, new Vector2(0, 50), 1);
             RaycastHit hit = new RaycastHit();
             bool hasHit = Physics.Raycast(Vector2.zero, Vector2.up, out hit, 100, 0);
             Assert.That(hasHit, Is.True);
@@ -101,8 +101,8 @@ namespace PressPlay.FFWD.Test.Physics_facts
         {
             Body body = Physics.AddBody();
             Body body1 = Physics.AddBody();
-            Physics.AddBox(body, false, 10, 10, new Vector2(0, 20), 0, 1);
-            Physics.AddBox(body1, false, 10, 10, new Vector2(0, 50), 0, 1);
+            Physics.AddBox(body, false, 10, 10, new Vector2(0, 20), 1);
+            Physics.AddBox(body1, false, 10, 10, new Vector2(0, 50), 1);
             RaycastHit hit = new RaycastHit();
             bool hasHit = Physics.Raycast(Vector2.zero, Vector2.up, out hit, 100, 0);
             Assert.That(hasHit, Is.True);
@@ -114,8 +114,8 @@ namespace PressPlay.FFWD.Test.Physics_facts
         {
             Body body = Physics.AddBody();
             Body body1 = Physics.AddBody();
-            Physics.AddBox(body1, false, 10, 10, new Vector2(0, 50), 0, 1);
-            Physics.AddBox(body, false, 10, 10, new Vector2(0, 20), 0, 1);
+            Physics.AddBox(body1, false, 10, 10, new Vector2(0, 50), 1);
+            Physics.AddBox(body, false, 10, 10, new Vector2(0, 20), 1);
             RaycastHit[] hits = Physics.RaycastAll(Vector2.zero, Vector2.up, 100, 0);
             Assert.That(hits.Length, Is.EqualTo(2));
         }
@@ -136,8 +136,8 @@ namespace PressPlay.FFWD.Test.Physics_facts
         {
             Body body = Physics.AddBody();
             Body body1 = Physics.AddBody();
-            Physics.AddBox(body1, false, 10, 10, new Vector2(0, 50), 0, 1);
-            Physics.AddBox(body, false, 10, 10, new Vector2(0, 20), 0, 1);
+            Physics.AddBox(body1, false, 10, 10, new Vector2(0, 50), 1);
+            Physics.AddBox(body, false, 10, 10, new Vector2(0, 20), 1);
             RaycastHit[] hits = Physics.RaycastAll(Vector2.zero, Vector2.up, 30, 0);
             Assert.That(hits.Length, Is.EqualTo(1));
         }
--- PressPlay.FFWD/Components/Colliders/MeshCollider.cs
+++ PressPlay.FFWD/Components/Colliders/MeshCollider.cs
@@ -16,59 +16,26 @@ namespace PressPlay.FFWD.Components
 
         internal override void AddCollider(Body body, float mass)
         {
-
-            bool useTriangles = true;
-
-            if (!transform.parent.name.Contains("entrance"))
+            List<Triangle> tris = new List<Triangle>();
+            for (int i = 0; i < triangles.Length; i += 3)
             {
-                useTriangles = false;
-            }
-
-            if (useTriangles)
-            {
-                List<Microsoft.Xna.Framework.Vector2[]> tris = new List<Microsoft.Xna.Framework.Vector2[]>();
-                for (int i = 0; i < triangles.Length; i += 3)
-                {
 #if DEBUG
-                if (vertices[triangles[i]].y + vertices[triangles[i + 1]].y + vertices[triangles[i + 2]].y > 1)
-                {
-                    Debug.Log(" Warning: " + ToString() + " has non zero Y in collider");
-                }
-#endif
-                    Microsoft.Xna.Framework.Vector2[] tri = new Microsoft.Xna.Framework.Vector2[] { 
-                    new Microsoft.Xna.Framework.Vector2(vertices[triangles[i]].x * transform.lossyScale.x, vertices[triangles[i]].z * transform.lossyScale.z),
-                    new Microsoft.Xna.Framework.Vector2(vertices[triangles[i + 2]].x * transform.lossyScale.x, vertices[triangles[i + 2]].z * transform.lossyScale.z),
-                    new Microsoft.Xna.Framework.Vector2(vertices[triangles[i + 1]].x * transform.lossyScale.x, vertices[triangles[i + 1]].z * transform.lossyScale.z)
-                };
-                    tris.Add(tri);
-                }
-
-                connectedBody = Physics.AddMesh(body, isTrigger, tris, mass);
-            }
-            else
+            if (vertices[triangles[i]].y + vertices[triangles[i + 1]].y + vertices[triangles[i + 2]].y > 1)
             {
-                List<Triangle> tris = new List<Triangle>();
-                for (int i = 0; i < triangles.Length; i += 3)
-                {
-#if DEBUG
-                if (vertices[triangles[i]].y + vertices[triangles[i + 1]].y + vertices[triangles[i + 2]].y > 1)
-                {
-                    Debug.Log(" Warning: " + ToString() + " has non zero Y in collider");
-                }
+                Debug.Log(" Warning: " + ToString() + " has non zero Y in collider");
+            }
 #endif
-                    Triangle tri = new Triangle(
-                        vertices[triangles[i]].x * transform.lossyScale.x, vertices[triangles[i]].z * transform.lossyScale.z,
-                        vertices[triangles[i + 2]].x * transform.lossyScale.x, vertices[triangles[i + 2]].z * transform.lossyScale.z,
-                        vertices[triangles[i + 1]].x * transform.lossyScale.x, vertices[triangles[i + 1]].z * transform.lossyScale.z
-                    );
-                    tris.Add(tri);
-                }
-
-                List<Vertices> verts = EarclipDecomposer.PolygonizeTriangles(tris, int.MaxValue, 0);
-
-                connectedBody = Physics.AddMesh(body, isTrigger, verts, mass);
+                Triangle tri = new Triangle(
+                    vertices[triangles[i]].x * transform.lossyScale.x, vertices[triangles[i]].z * transform.lossyScale.z,
+                    vertices[triangles[i + 2]].x * transform.lossyScale.x, vertices[triangles[i + 2]].z * transform.lossyScale.z,
+                    vertices[triangles[i + 1]].x * transform.lossyScale.x, vertices[triangles[i + 1]].z * transform.lossyScale.z
+                );
+                tris.Add(tri);
             }
 
+            List<Vertices> verts = EarclipDecomposer.PolygonizeTriangles(tris, int.MaxValue, 0);
+
+            connectedBody = Physics.AddMesh(body, isTrigger, verts, mass);
         }
     }
 }
