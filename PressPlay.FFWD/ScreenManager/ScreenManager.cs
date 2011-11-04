dace92b967cd5b86fc81b4c7121ad11b3b0af4e7
Thomas Gravgaard <thomas@pressplay.dk>
2011-11-04 12:50:27 +0100

    #79: The popup now stays up, and the pause menu is not added twice

--- PressPlay.FFWD/ScreenManager/ScreenManager.cs
+++ PressPlay.FFWD/ScreenManager/ScreenManager.cs
@@ -308,6 +308,11 @@ namespace PressPlay.FFWD.ScreenManager
         /// </summary>
         public void AddScreen(GameScreen screen, PlayerIndex? controllingPlayer)
         {
+            if (screens.Contains(screen))
+            {
+                return;
+            }
+
             //log add screen
             LogAction(screen.GetType().Name);
 
@@ -357,6 +362,8 @@ namespace PressPlay.FFWD.ScreenManager
         /// </summary>
         public void RemoveScreen(GameScreen screen)
         {
+            Debug.Log("Remove screen " + screen.GetType().Name);
+
             // If we have a graphics device, tell the screen to unload content.
             if (isInitialized)
             {
