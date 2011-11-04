02acda0e42492536ebc437938c61f44b49f9ea93
Thomas Gravgaard <thomas@pressplay.dk>
2011-11-04 13:57:46 +0100

    Gray out text if button is disabled

--- PressPlay.FFWD/UI/Controls/ButtonControl.cs
+++ PressPlay.FFWD/UI/Controls/ButtonControl.cs
@@ -125,6 +125,15 @@ namespace PressPlay.FFWD.UI.Controls
 
             previousState = _state;
             _state = newState;
+
+            if (newState == ButtonControlStates.disabled)
+            {
+                textControl.SetColor(FFWD.Color.gray);
+            }
+            if (previousState == ButtonControlStates.disabled)
+            {
+                textControl.SetColor(FFWD.Color.white);
+            }
         }
 
         public void ScaleTextToFit()
