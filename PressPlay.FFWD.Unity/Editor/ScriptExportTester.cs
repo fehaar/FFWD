dd774ea7129d8f315aeaf26d9c10370ccc69eb52
Thomas Gravgaard <thomas@pressplay.dk>
2011-11-25 11:35:27 +0100

    Upgrede FFWD to .NET 3.5 and add the newest Unity DLLs

---
+++ PressPlay.FFWD.Unity/Editor/ScriptExportTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+public class ScriptExportTester
+{
+    [MenuItem("CONTEXT/MonoBehaviour/FFWD Export Script - EXPERIMENTAL")]
+    static void ExportScript(MenuCommand command)
+    {
+        ExportSceneWizard wiz = new ExportSceneWizard();
+        
+    }
+}
