5ca87cd97f50d0358cd3c2030d92b9ef4cdfcd17
Thomas Gravgaard <thomas@pressplay.dk>
2011-12-21 16:12:09 +0100

    Call OnEnable, OnDisable and OnDestroy on Scriptable objects.

--- PressPlay.FFWD/ScriptableObject.cs
+++ PressPlay.FFWD/ScriptableObject.cs
@@ -9,19 +9,29 @@ namespace PressPlay.FFWD
     {
         public static T CreateInstance<T>() where T : ScriptableObject
         {
-            return Activator.CreateInstance<T>();
+            ScriptableObject obj = Activator.CreateInstance<T>();
+            if (obj != null)
+	        {
+                obj.OnEnable();
+	        }
+            return obj as T;
         }
 
         public static ScriptableObject CreateInstance(Type type)
         {
-            return (ScriptableObject)Activator.CreateInstance(type);
+            ScriptableObject obj = (ScriptableObject)Activator.CreateInstance(type);
+            if (obj != null)
+            {
+                obj.OnEnable();
+            }
+            return obj;
         }
 
         public static ScriptableObject CreateInstance(string typeName)
         {
             throw new NotImplementedException("Somethings is off here. We don't find the types involved...");
-            Type t = Type.GetType(typeName);
-            return CreateInstance(t);
+            //Type t = Type.GetType(typeName);
+            //return CreateInstance(t);
         }
 
         public virtual void OnEnable()
@@ -32,6 +42,18 @@ namespace PressPlay.FFWD
         public virtual void OnDisable()
         {
             // NOTE: Do not make any code here. Typically base method is NOT called in MonoScripts so this will not be called either!!!!!
+        }
+
+        public virtual void OnDestroy()
+        {
+            // NOTE: Do not make any code here. Typically base method is NOT called in MonoScripts so this will not be called either!!!!!
+        }
+
+        protected override void Destroy()
+        {
+            OnDisable();
+            OnDestroy();
+            base.Destroy();
         }
     }
 }
