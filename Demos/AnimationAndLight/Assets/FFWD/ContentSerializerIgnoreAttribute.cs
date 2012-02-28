b2bba09366729c5a932d84e52785962678012255
Thomas Gravgaard <thomas@pressplay.dk>
2012-02-28 23:12:01 +0100

    Added a basic demo, which show an error in lighting with the DynamicBatchRenderer.

---
+++ Demos/AnimationAndLight/Assets/FFWD/ContentSerializerIgnoreAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// This attribute controls that XNA will not try to load this member from XML. It will also not be exported.
+/// At the moment it is required on all properties as XNA will try to serialize them in if they are not marked.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+sealed class ContentSerializerIgnoreAttribute : Attribute
+{
+}
