187e1d28398684a93a8e699dee8e8d59efefb21b
Thomas Gravgaard <thomas@pressplay.dk>
2012-03-15 16:58:05 +0100

    Renamed Particle exporter and made the exporter use it.

---
+++ Exporter/PressPlay.FFWD.Unity/FFWD_EllipsoidParticleEmitterExport.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+[FFWD_DontExport]
+public class FFWD_EllipsoidParticleEmitterExport : MonoBehaviour
+{
+	public Vector3 ellipsoid;
+	public bool oneShot = false;
+	public Vector3 tangentVelocity;
+	public float minEmitterRange;
+	
+	public StretchParticles stretchParticles = StretchParticles.Billboard;
+	
+	public enum StretchParticles{
+		Billboard,
+		Stretched,
+		HorizontalBillboard
+	}
+	
+	// Use this for initialization
+	void Awake () {
+		Destroy(this);
+	}
+}
